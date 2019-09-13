ed86d01e7eb1a6ac2a587a27acef07dfd8245c94
Komi <mickael@ermes.ai>
2019-09-13 10:04:38 +0200

    flask form

--- API/server.py
+++ API/server.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world'
+    return render_template('index.html')
 
 @app.route('/bonjour')
 def autre_route():
     return 'Bonjour'
 
+@app.route('/handle_form', methods=['POST'])
+def handle_form():
+    print(request.form.get('first_name'))
+    return 'Bonjour'
+
+@app.route('/projet/<name>')
+def projet(name):
+    return 'Bonjour '+name
 
 app.run(port=3000, debug=True)
